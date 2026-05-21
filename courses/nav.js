85e1d7b7882b7a9a08555e5fe40db08f5ca4a5d3
Jasbi <6220536+jasbi@users.noreply.github.com>
2026-05-21 10:28:27 -0700

    layout navigation overhaul

---
+++ courses/nav.js
@@ -0,0 +1,15 @@
+document.write(`<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-faded" style="background: rgb(2,40,81)">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand nav-bracket" href="../index.html">MASOUD JASBI</a>
+    <div class="collapse navbar-collapse justify-content-end" id="nav-content">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link nav-bracket" href="../research.html">RESEARCH</a></li>
+            <li class="nav-item"><a class="nav-link nav-bracket" href="../teaching.html">TEACHING</a></li>
+            <li class="nav-item"><a class="nav-link nav-bracket" href="../service.html">SERVICE</a></li>
+            <li class="nav-item"><a class="nav-link nav-bracket" href="../jasbi-cv.pdf">CV (PDF)</a></li>
+            <li class="nav-item"><a class="nav-link nav-bracket" href="https://l3lab.ucdavis.edu/">L3 LAB</a></li>
+        </ul>
+    </div>
+</nav>`);
